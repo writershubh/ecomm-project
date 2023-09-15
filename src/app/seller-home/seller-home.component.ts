d044cf442e15f9a559d9fa6028fa8c30d25a7af0
Shubham Kumar <srajsam.sa@gmail.com>
2023-09-15 23:43:51 +0530

    till seller product list edit and update

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { product } from '../data-type';
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
   styleUrls: ['./seller-home.component.css']
 })
-export class SellerHomeComponent implements OnInit{
+export class SellerHomeComponent implements OnInit {
 
   productList: undefined | product[];
+  productMsg: undefined | string;
+  deleteIcon = faTrash;
+  editIcon = faEdit;
 
-  constructor(private product: ProductService) {}
+  constructor(private product: ProductService) { }
 
   ngOnInit(): void {
+    this.productLists();
+  }
+
+  deleteProduct = (id: number) => {
+    this.product.deleteProduct(id).subscribe((result) => {
+      if (result) {
+        this.productMsg = 'Product deleted.';
+        this.productLists();
+      }
+    });
+    setTimeout(() => this.productMsg = undefined, 3000);
+  }
+
+  productLists = () => {
     this.product.productList().subscribe((result) => {
-      this.productList = result;
+      if (result) {
+        this.productList = result;
+      }
     });
   }
 }
---
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
+
+@Component({
+  selector: 'app-seller-update-product',
+  templateUrl: './seller-update-product.component.html',
+  styleUrls: ['./seller-update-product.component.css']
+})
+export class SellerUpdateProductComponent implements OnInit {
+
+  productData: undefined | product;
+  productMsg: undefined | string;
+
+  constructor(private activateRoute: ActivatedRoute, private productService: ProductService) { }
+
+  ngOnInit(): void {
+    let productId = this.activateRoute.snapshot.paramMap.get('id');
+    productId && this.productService.getProduct(productId).subscribe((data) => {
+      this.productData = data;
+    });
+  }
+
+  submitupdateProduct = (data: product) => {
+    if (this.productData){
+      data.id = this.productData.id;
+    }
+    this.productService.updateProduct(data).subscribe((result) => {
+      if (result) {
+        this.productMsg = 'Product updated.'
+      }
+    });
+    setTimeout(() => {
+      this.productMsg = undefined
+    }, 3000);
+  }
+}
