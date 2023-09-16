9106297e5fb093ebbf3280a85a36a9c22dc4c404
Shubham Kumar <srajsam.sa@gmail.com>
2023-09-17 00:14:24 +0530

    implementation of search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit{
 
+  searchResult: undefined | product[];
+
+  constructor(private activeRoute: ActivatedRoute, private productService: ProductService) {}
+
+  ngOnInit(): void {
+    let query = this.activeRoute.snapshot.paramMap.get('query');
+    query && this.productService.searchProducts(query).subscribe((result) => {
+      this.searchResult = result;
+    });
+  }
 }
