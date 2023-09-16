648b60ee5629b94ba167a56c799647556dcac2c9
Shubham Kumar <srajsam.sa@gmail.com>
2023-09-16 22:49:20 +0530

    added footer, search component

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { faFacebook, faTwitter, faGoogle, faInstagram, faLinkedin, faGithub, faApple, faAndroid, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faHouse, faPhone } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
+  faGoogle = faGoogle;
+  faInstagram = faInstagram;
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faEnvelope = faEnvelope;
+  faHouse = faHouse;
+  faPhone = faPhone;
+  faApple = faApple;
+  faAndroid = faAndroid;
+  faYoutube = faYoutube;
+}
